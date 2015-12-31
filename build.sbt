e11a8aa597926dd6b19745fa04e485eb95a2116e
sudhanshucena <s1459349@sms.ed.ac.uk>
2015-12-31 21:16:31 +0000

    Added the spark core and base code

--- build.sbt
+++ build.sbt
@@ -4,4 +4,5 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+libraryDependencies ++= Seq("org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+            "org.apache.spark" % "spark-core_2.11" % "1.5.2")
--- src/main/scala-2.11/DriverProgram.scala
+++ src/main/scala-2.11/DriverProgram.scala
@@ -1,6 +1,16 @@
 /**
   * Created by sudha on 12/30/2015.
   */
+
+import org.apache.spark.SparkConf
+import org.apache.spark.{SparkContext, SparkConf}
 class DriverProgram {
+
+  def main(args: Array[String]){
+    val conf = new SparkConf().setMaster("local").setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+
+
+  }
 
 }
