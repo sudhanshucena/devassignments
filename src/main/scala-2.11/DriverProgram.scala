baeb60edbaa4654b72075478690a6b625925d7ff
sudhanshucena <s1459349@sms.ed.ac.uk>
2015-12-31 22:02:26 +0000

    extarcting all the three digit numbers

--- src/main/scala-2.11/DriverProgram.scala
+++ src/main/scala-2.11/DriverProgram.scala
@@ -4,13 +4,15 @@
 
 import org.apache.spark.SparkConf
 import org.apache.spark.{SparkContext, SparkConf}
-class DriverProgram {
+object DriverProgram {
 
   def main(args: Array[String]){
     val conf = new SparkConf().setMaster("local").setAppName("Simple Application")
     val sc = new SparkContext(conf)
 
-
+    val logData = sc.textFile("/home/sudhanshu/Desktop/devassignments/resources/mock_apache_pool-1-thread-1.data")
+    val splitLogData = logData.flatMap(line => line.split(" +")).filter(_.matches("[0-9]{3}"))
+    splitLogData.take(20).foreach(println)
   }
 
 }
