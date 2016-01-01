c449216eb0b15c2fe6181e7083d080fdea34b063
sudhanshucena <sudhanshucena@gmail.com>
2016-01-01 10:18:51 +0000

    Completed task 3

--- src/main/scala-2.11/DriverProgram.scala
+++ src/main/scala-2.11/DriverProgram.scala
@@ -11,8 +11,9 @@ object DriverProgram {
     val sc = new SparkContext(conf)
 
     val logData = sc.textFile("/home/sudhanshu/Desktop/devassignments/resources/mock_apache_pool-1-thread-1.data")
-    val splitLogData = logData.flatMap(line => line.split(" +")).filter(_.matches("[0-9]{3}"))
-    splitLogData.take(20).foreach(println)
+    val splitLogData = logData.flatMap(line => line.split(" +")).filter(_.matches("\\b(200|404|503)\\b"))
+    val results = splitLogData.map(statusCode => (statusCode,1)).reduceByKey((count1,count2)=>count1+count2)
+    results.collect().foreach(println)
   }
 
 }
